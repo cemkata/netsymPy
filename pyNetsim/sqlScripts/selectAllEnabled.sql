3967bd3ca560899daf4600ce1f567b7045ed4a7c
cemkata <cemkata@users.noreply.github.com>
2017-07-06 17:23:47 +0300

    Delete selectAllEnabled.sql

--- pyNetsim/sqlScripts/selectAllEnabled.sql
+++
@@ -1,4 +0,0 @@
-select `selIntface`, `txtBufferLimit`, `txtBandwidth`, `selDelayDistribution`, `txtDelay`, `txtDelayJitter`, `txtDelayCorrelation`,
-`txtReorder`, `txtReorderCorrelation`, `txtGap`, `txtLoss`, `txtLossCorrelation`, `txtDup`, `txtDupCorrelation`, `txtCurp`,
-`txtCurptionCorrelation`, `portSrc`, `portDst`, `macSrc`, `macDest`, `flowlabelTOS`, `transport`, `transportPrtc`,`ipSrc`,
-`ipSrcSub`, `ipDest`, `ipDestSub`, `ipVer`, `ruleStatus`,`id` from `rules_tbl` where id != '1' and ruleStatus == "On"
